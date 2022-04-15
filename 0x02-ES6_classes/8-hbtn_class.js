dd33f313034c813073efa112aaabfa998244d848
Mateo <mateog91@gmail.com>
2022-04-14 23:48:16 -0500

    FIX print

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -12,7 +12,7 @@ export default class Airport {
 
   set location(value) { this._location = value; }
 
-  Number() { return `${this.size}`; }
+  Number() { return this.size; }
 
   String() { return `${this.location}`; }
 }
