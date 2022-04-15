e3d01ea388affe6b56552917aaf1dbff73290ef7
Mateo <mateog91@gmail.com>
2022-04-14 22:44:03 -0500

    FIX STYLE

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -4,13 +4,13 @@ export default class Pricing {
     this._currency = currency;
   }
 
-  get amount() { return this._amount; };
+  get amount() { return this._amount; }
 
-  set amount(amount) { this._amount = amount; };
+  set amount(amount) { this._amount = amount; }
 
-  get currency() { return this._currency; };
+  get currency() { return this._currency; }
 
-  set currency(currency) { this._currency = currency; };
+  set currency(currency) { this._currency = currency; }
 
   displayFullPrice() {
     return `${this.amount} ${this.currency.name} (${this.currency.code})`;
