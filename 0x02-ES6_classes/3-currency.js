a6bb595e8ad1f8ad746176f52b0a8eebd331a8c8
Mateo <mateog91@gmail.com>
2022-04-14 22:45:13 -0500

    FIX STYLE

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,25 +1,26 @@
 export default class Currency {
-	constructor(code, name) {
-		this._code = code;
-		this._name = name;
-	}
-	get code() {
-		return this._code
-	}
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-	set code(value) {
-		this._code = value;
-	}
+  get code() {
+    return this._code;
+  }
 
-	get name() {
-		return this._name;
-	}
+  set code(value) {
+    this._code = value;
+  }
 
-	set name(value) {
-		this._name = value;
-	}
+  get name() {
+    return this._name;
+  }
 
-	displayFullCurrency() {
-		return `${this.name} (${this.code})`
-	}
+  set name(value) {
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
