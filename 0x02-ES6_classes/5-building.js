83cbab8c5df67f7860b24d798082a13a6d0acd5e
Mateo <mateog91@gmail.com>
2022-04-14 22:38:16 -0500

    FIX: style

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,16 @@
 export default class Building {
-	constructor(sqft) {
-		if (this.constructor != Building) {
-			if (!('evacuationWarningMessage' in this.constructor)) {
-			}
-			throw new Error('Class extending Building must override evacuationWarningMessage')
-		}
-		this._sqft = sqft;
-		}
-		get sqft() { return this._sqft; };
-		set sqft(value) { this._sqft = value; };
-	}
+  constructor(sqft) {
+    this._sqft = sqft;
+    // console.log('here is', this)
+    // console.log(!('evacuationWarningMessage' in this))
+    if (this.constructor !== Building) {
+      if (!('evacuationWarningMessage' in this)) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
 
+  get sqft() { return this._sqft; }
 
+  set sqft(value) { this._sqft = value; }
+}
